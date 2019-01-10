047aaf5e3a1dd8d25bc6475c4e5442aec4866d32
BridgeLabz <rahul.mandalkar@bridgelabz.com>
2019-01-10 18:12:53 +0530

    Added firebase service json

--- models/Label.js
+++ models/Label.js
@@ -3,7 +3,6 @@ const  LabelSchema = new Schema({
     title: { type: String, default: "" },
     createDate:{ type: String, default: Date.now },
     editDate:{ type: String, default: Date.now },
-    note:{type:mongoose.Schema.ObjectId, ref:'notes'},
-   
+    note:{type:mongoose.Schema.ObjectId, ref:'notes'}
 })
 module.exports = mongoose.model("labels", LabelSchema);
