a79db63db15b6148ff3f90b4fa17341a02d16199
BridgeLabz <rahul.mandalkar@bridgelabz.com>
2019-01-10 18:07:37 +0530

    Changed Label Schema

--- models/Label.js
+++ models/Label.js
@@ -3,6 +3,7 @@ const  LabelSchema = new Schema({
     title: { type: String, default: "" },
     createDate:{ type: String, default: Date.now },
     editDate:{ type: String, default: Date.now },
-    note:{type:mongoose.Schema.ObjectId, ref:'notes'}
+    note:{type:mongoose.Schema.ObjectId, ref:'notes'},
+    user:{type:mongoose.Schema.ObjectId, ref:'users'}
 })
 module.exports = mongoose.model("labels", LabelSchema);
